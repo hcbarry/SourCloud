e171b0af58856dc19c823e76b49177f12814b785
Barry Lee <barry@17.media>
2021-01-27 00:07:27 +0800

    Backward compatiability

--- SourCloud/main.m
+++ SourCloud/main.m
@@ -9,11 +9,9 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
-    NSString * appDelegateClassName;
+int main(int argc, char *argv[])
+{
     @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+        return UIApplicationMain(argc, argv, @"UIApplication", @"AppDelegate");
     }
-    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
